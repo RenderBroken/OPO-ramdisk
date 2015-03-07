67e17cb44ab332d8e8ff8bbf207dcdbef5bac8fc
Zachariah Kennedy <zkennedy87@gmail.com>
2015-03-07 11:27:33 -0500

    Setup Render Script
    Signed-off-by: Zachariah Kennedy <zkennedy87@gmail.com>

---
+++ init.render.post_boot.sh
@@ -0,0 +1,50 @@
+#!/system/bin/sh
+
+BB=/sbin/busybox
+
+############################
+# Custom Kernel Settings for Render Kernel!!
+#
+stop mpdecision
+
+############################
+# MSM_Hotplug Settings
+#
+echo 2 > /sys/module/msm_hotplug/cpus_boosted
+echo 750 > /sys/module/msm_hotplug/down_lock_duration
+echo 2500 > /sys/module/msm_hotplug/boost_lock_duration
+echo 200 5:100 50:50 350:200 > /sys/module/msm_hotplug/update_rates
+echo 400 > /sys/module/msm_hotplug/fast_lane_load
+echo 1 > /sys/module/msm_hotplug/max_cpus_online_susp
+
+############################
+# LMK Tweaks
+#
+echo 2560,4096,8192,16384,24576,32768 > /sys/module/lowmemorykiller/parameters/minfree
+echo 32 > /sys/module/lowmemorykiller/parameters/cost
+
+############################
+# Tweak Background Writeout
+#
+echo 200 > /proc/sys/vm/dirty_expire_centisecs
+echo 40 > /proc/sys/vm/dirty_ratio
+echo 5 > /proc/sys/vm/dirty_background_ratio
+echo 0 > /proc/sys/vm/swappiness
+
+############################
+# Power Effecient Workqueues (Enable for battery)
+#
+echo 1 > /sys/module/workqueue/parameters/power_efficient
+
+############################
+# MSM Limiter
+#
+echo 300000 > /sys/kernel/msm_limiter/suspend_min_freq_0
+echo 300000 > /sys/kernel/msm_limiter/suspend_min_freq_1
+echo 300000 > /sys/kernel/msm_limiter/suspend_min_freq_2
+echo 300000 > /sys/kernel/msm_limiter/suspend_min_freq_3
+echo 2457600 > /sys/kernel/msm_limiter/resume_max_freq_0
+echo 2457600 > /sys/kernel/msm_limiter/resume_max_freq_1
+echo 2457600 > /sys/kernel/msm_limiter/resume_max_freq_2
+echo 2457600 > /sys/kernel/msm_limiter/resume_max_freq_3
+echo 1267200 > /sys/kernel/msm_limiter/suspend_max_freq
