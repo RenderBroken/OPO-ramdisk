7e51b87874838a44bc78cc360bdb4789cd6f81a5
Zachariah <zkennedy87@gmail.com>
2015-03-22 11:31:12 -0400

    Render Script: Make sure GPU boots at Stock Freqs
    Signed-off-by: Zachariah <zkennedy87@gmail.com>

--- init.render.post_boot.sh
+++ init.render.post_boot.sh
@@ -15,7 +15,7 @@ echo 750 > /sys/module/msm_hotplug/down_lock_duration
 echo 2500 > /sys/module/msm_hotplug/boost_lock_duration
 echo 200 5:100 50:50 350:200 > /sys/module/msm_hotplug/update_rates
 echo 400 > /sys/module/msm_hotplug/fast_lane_load
-echo 1 > /sys/module/msm_hotplug/max_cpus_online_susp
+echo 2 > /sys/module/msm_hotplug/max_cpus_online_susp
 
 ############################
 # LMK Tweaks
@@ -29,7 +29,7 @@ echo 32 > /sys/module/lowmemorykiller/parameters/cost
 echo 200 > /proc/sys/vm/dirty_expire_centisecs
 echo 40 > /proc/sys/vm/dirty_ratio
 echo 5 > /proc/sys/vm/dirty_background_ratio
-echo 0 > /proc/sys/vm/swappiness
+echo 10 > /proc/sys/vm/swappiness
 
 ############################
 # Power Effecient Workqueues (Enable for battery)
@@ -47,4 +47,15 @@ echo 2457600 > /sys/kernel/msm_limiter/resume_max_freq_0
 echo 2457600 > /sys/kernel/msm_limiter/resume_max_freq_1
 echo 2457600 > /sys/kernel/msm_limiter/resume_max_freq_2
 echo 2457600 > /sys/kernel/msm_limiter/resume_max_freq_3
-echo 1267200 > /sys/kernel/msm_limiter/suspend_max_freq
+echo 1728000 > /sys/kernel/msm_limiter/suspend_max_freq
+
+############################
+# Scheduler and Read Ahead
+#
+echo zen > /sys/block/mmcblk0/queue/scheduler
+echo 2048 > /sys/block/mmcblk0/bdi/read_ahead_kb
+
+############################
+# GPU Governor
+#
+echo 578000000 > /sys/devices/fdb00000.qcom,kgsl-3d0/devfreq/fdb00000.qcom,kgsl-3d0/max_freq
